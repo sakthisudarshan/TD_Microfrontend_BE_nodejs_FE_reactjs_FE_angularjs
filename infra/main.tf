0c9e6014659f8b8bad557d536b98ddf12b843f0d
Mohammed-shihaf <shihaf@Testable.Cloud>
2026-08-13 15:28:23 +0530

    microfrontend_BE_nodejs24_FE_reactjs24_FE_angularjs19: add shell script (bash-version-guarded), IaC (Dockerfile+k8s+Terraform), and SOC2 doc for readiness scanner

---
+++ infra/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "microfrontend_backend" {
+  name = "microfrontend-backend:node24"
+  build {
+    context    = "${path.module}/.."
+    dockerfile = "infra/Dockerfile"
+  }
+}
+
+resource "docker_container" "microfrontend_backend" {
+  name  = "microfrontend-backend"
+  image = docker_image.microfrontend_backend.image_id
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
